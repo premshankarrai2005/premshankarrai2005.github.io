33b62004b5eb95e7393d5ed5e87cf2b1a354dfd9
Prem Shankar Rai <premshankarrai2005@gmail.com>
2026-01-10 10:17:38 +0530

    vite config file changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
+  base: '/', // ✅ MUST be root for custom domain + username.github.io
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
